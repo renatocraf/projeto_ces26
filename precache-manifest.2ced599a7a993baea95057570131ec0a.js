ffd7d600900306405f235fab831a2174f064060b
caio-ggomes <caio.graca@gmail.com>
2020-10-21 00:11:38 -0300

    Updates

---
+++ precache-manifest.2ced599a7a993baea95057570131ec0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b731d1288b161e3aaf862bf67971581e",
+    "url": "/projeto_ces26/index.html"
+  },
+  {
+    "revision": "90c399c19bf7a4bfe215",
+    "url": "/projeto_ces26/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff6abf3db57240695eea",
+    "url": "/projeto_ces26/static/js/2.7eb0f0b0.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/projeto_ces26/static/js/2.7eb0f0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c399c19bf7a4bfe215",
+    "url": "/projeto_ces26/static/js/main.da6e9806.chunk.js"
+  },
+  {
+    "revision": "66806c754ad402771d14",
+    "url": "/projeto_ces26/static/js/runtime-main.3f50812d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projeto_ces26/static/media/logo.5d5d9eef.svg"
+  }
+]);
